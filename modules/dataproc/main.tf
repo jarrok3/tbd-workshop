9bbfaed52a1127b15cc54125cdd6cc7160778936
JNajdek <161584862+JNajdek@users.noreply.github.com>
2026-04-15 16:18:56 +0200

    adding main.tf to master

--- modules/dataproc/main.tf
+++ modules/dataproc/main.tf
@@ -134,5 +134,12 @@ resource "google_dataproc_cluster" "tbd-dataproc-cluster" {
       }
 
     }
+    preemptible_worker_config {
+      num_instances = 2
+      disk_config {
+        boot_disk_type    = "pd-standard"
+        boot_disk_size_gb = 100
+      }
+    }
   }
 }
